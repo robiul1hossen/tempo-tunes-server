1ecaaf9a3ce21f7ea7c2ee8554338d794e77387e
robiul1hossen <robim6225@gmail.com>
2023-06-10 11:44:56 +0600

    created post method for add class by instructor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,21 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unauthorized access " });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "unauthorized access " });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("tempo is running");
@@ -33,12 +49,32 @@ async function run() {
     const classCollection = client.db("tempoTunes").collection("classes");
     const studentCollection = client.db("tempoTunes").collection("students");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      res.send(token);
+    });
+
     app.get("/classes", async (req, res) => {
+      //   const options = {
+      //     sort: { seats: ass === "ass" ? 1 : -1 },
+      //   };
       const result = await classCollection.find().toArray();
       res.send(result);
     });
 
+    app.post("/classes", async (req, res) => {
+      const newClass = req.body;
+      const result = await classCollection.insertOne(newClass);
+      res.send(result);
+    });
+
     // students api
+    app.get("/allusers", async (req, res) => {
+      const result = await studentCollection.find().toArray();
+      res.send(result);
+    });
+
     app.get("/students", async (req, res) => {
       const result = await studentCollection.find().toArray();
       res.send(result);
