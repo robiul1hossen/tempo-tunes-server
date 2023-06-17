5692f179110e27b30814895b45a0e3b1c28d58c1
robiul1hossen <robim6225@gmail.com>
2023-06-17 13:48:51 +0600

    some data chages

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const classCollection = client.db("tempoTunes").collection("classes");
     const studentCollection = client.db("tempoTunes").collection("students");
@@ -78,7 +78,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/classes", verifyJWT, async (req, res) => {
+    app.get("/classes", async (req, res) => {
       const result = await classCollection.find().limit(6).toArray();
       res.send(result);
     });
@@ -260,7 +260,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
